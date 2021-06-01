a92abc51138a2359beae7797cab75b6f3088ae98
ryan <hopelessgains@gmail.com>
2021-06-01 13:22:00 -0600

    added  app.js code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import { ballClock, results } from "./function";
+import "./App.css";
 
 function App() {
+  const [form, setValues] = useState({ input: "" });
+
+  const handleChange = (e) =>
+    setValues({
+      ...form,
+      [e.target.name]: e.target.value,
+    });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    ballClock(form.input);
+    setValues({ ...form, input: "" });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Ball Clock</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          className="input"
+          type="input"
+          name="input"
+          placeholder="Input Ball Count"
+          value={form.input}
+          onChange={handleChange}
+        ></input>
+        <button className="button" type="submit">
+          Submit
+        </button>
+      </form>
+      <br />
+
+      <h3>{results}</h3>
     </div>
   );
 }
